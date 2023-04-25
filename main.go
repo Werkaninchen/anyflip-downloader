7ba712de128ae979e3640f3b761b49202928aaae
Jeroen Trzaska <jeroen@trzaska.xyz>
2023-04-25 15:50:10 -0600

    added ocr option reliant on tesseract and qpdf

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path"
 	"regexp"
 	"strconv"
@@ -19,10 +20,14 @@ import (
 )
 
 func main() {
+
 	anyflipURL, err := url.Parse(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//my pref
+	os.Args = append(os.Args, "--extract_title", "--ocr")
 
 	bookUrlPathElements := strings.Split(anyflipURL.Path, "/")
 	// secect only 1st and 2nd element of url to avoid mobile on online.anyflip urls
@@ -34,11 +39,14 @@ func main() {
 
 	// parse --extract_title to automatically rename pdf to it's title from anyflip
 	if len(os.Args) > 2 && slices.Contains(os.Args, "--extract_title") {
-		outputFile, err = getBookTitle(anyflipURL)
+		var of = ""
+		of, err = getBookTitle(anyflipURL)
 		if err != nil {
 			log.Fatal(err)
 		}
-		outputFile += ".pdf"
+		if of != "" {
+			outputFile = of + ".pdf"
+		}
 	}
 
 	fmt.Println("Preparing to download")
@@ -51,12 +59,70 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("Converting to pdf")
-	err = createPDF(outputFile, downloadFolder)
+	if len(os.Args) > 2 && slices.Contains(os.Args, "--ocr") {
+		err = createOCRPDF(outputFile, downloadFolder)
+	} else {
+		err = createPDF(outputFile, downloadFolder)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	os.RemoveAll(downloadFolder)
+	os.RemoveAll(downloadFolder + "_pdf")
+}
+
+// depends on tesseract and qpdf
+func createOCRPDF(outputFile string, imgDir string) error {
+	entries, err := os.ReadDir(imgDir)
+	if err != nil {
+		return err
+	}
+	bar := progressbar.NewOptions(len(entries),
+		progressbar.OptionFullWidth(),
+		progressbar.OptionSetPredictTime(false),
+		progressbar.OptionShowCount(),
+		progressbar.OptionSetDescription("OCR"),
+	)
+
+	os.Mkdir(imgDir+"_pdf", os.ModePerm)
+	//ocr
+	bar.Add(0)
+	for entrie := range entries {
+		entrieName := entries[entrie].Name()
+
+		cmd := exec.Command("tesseract", entrieName, "../"+imgDir+"_pdf/"+entrieName, "pdf")
+		cmd.Dir = imgDir
+
+		err = cmd.Run()
+		if err != nil {
+			return err
+		}
+		bar.Add(1)
+
+	}
+
+	pdf_entries, err := os.ReadDir(imgDir + "_pdf")
+	if err != nil {
+		return err
+	}
+
+	args := []string{"--empty", "--pages"}
+
+	for pdf_entrie := range pdf_entries {
+		args = append(args, pdf_entries[pdf_entrie].Name())
+	}
+	args = append(args, "--", "../"+outputFile)
+
+	cmd := exec.Command("qpdf", args...)
+	cmd.Dir = imgDir + "_pdf"
+
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func createPDF(outputFile string, imageDir string) error {
@@ -90,6 +156,7 @@ func downloadImages(url *url.URL, pageCount int, downloadFolder string) error {
 	}
 
 	for page := 1; page <= pageCount; page++ {
+
 		downloadURL.Path = path.Join(url.Path, "files", "mobile", strconv.Itoa(page)+".jpg")
 		response, err := http.Get(downloadURL.String())
 		if err != nil {
@@ -125,7 +192,18 @@ func getBookTitle(url *url.URL) (string, error) {
 		return url.String(), err
 	}
 	r := regexp.MustCompile("\"?(bookConfig\\.)?bookTitle\"?=\"(.*?)\"")
+
 	match := r.FindString(configjs)
+	if match == "" {
+		r = regexp.MustCompile(`"meta":\{"title":"(.*?)"`)
+	}
+
+	// fmt.Println(configjs)
+	match = r.FindString(configjs)
+	if match == "" {
+		return url.String(), err
+	}
+
 	match = match[22 : len(match)-1]
 	return match, nil
 }
